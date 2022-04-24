013ec2bbd744bf21f8c3f47f69e9fca87c21d917
Nikita Gorbachev <nikitagorbachev1997@gmail.com>
2022-04-24 12:23:38 +0300

    fix: fixed window width depending on presence of scrollbar

--- shelter/js/util.js
+++ shelter/js/util.js
@@ -1,30 +1,34 @@
- function getRandomInteger(min, max) {
-   min = Math.ceil(min < max ? min : max);
-   max = Math.floor(max > min ? max : min);
-   return Math.floor(Math.random() * (max - min)) + min;
- }
+function getRandomInteger(min, max) {
+  min = Math.ceil(min < max ? min : max);
+  max = Math.floor(max > min ? max : min);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function debounce(cb, delay) {
-  let timer = null;
-  return () => {
-    clearTimeout(timer);
-    timer = setTimeout(cb, delay);
-  }
+ let timer = null;
+ return () => {
+   clearTimeout(timer);
+   timer = setTimeout(cb, delay);
+ }
 }
 
 function shuffle(array) {
-  for (let i = 1; i < array.length; i++) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
+ for (let i = 1; i < array.length; i++) {
+   let j = Math.floor(Math.random() * (i + 1));
+   [array[i], array[j]] = [array[j], array[i]];
+ }
+ return array;
 }
 
 function blockScroll() {
-  document.documentElement.style.overflow = 'hidden'
+ const windowWidth = document.documentElement.clientWidth;
+ document.documentElement.style.overflow = 'hidden';
+ document.documentElement.style.paddingRight = `${document.documentElement.clientWidth-windowWidth}px`;
 }
+
 function unlockScroll() {
-  document.documentElement.style.overflow = '';
+ document.documentElement.style.overflow = '';
+ document.documentElement.style.padding = "0px";
 }
 
 export {getRandomInteger, debounce, shuffle, blockScroll, unlockScroll};
